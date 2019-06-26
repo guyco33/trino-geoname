eecb7d8de57645745f241363c275a54d3258fd78
Guy Cohen <guyc@gettaxi.com>
2019-06-26 14:33:28 +0300

    Add geoname function to retrive all attributes as a json string

--- src/main/java/io/prestosql/geoname/functions/GeonameFunctions.java
+++ src/main/java/io/prestosql/geoname/functions/GeonameFunctions.java
@@ -12,29 +12,42 @@ import io.prestosql.spi.type.StandardTypes;
 
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.prestosql.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
+import static io.prestosql.spi.type.StandardTypes.VARCHAR;
 import static java.lang.String.format;
 
 public class GeonameFunctions {
 
-    private static final String attributes_desc = "countrycode | city | timezone | admin1 | admin2";
+    private static final String attributes_desc = "countrycode | city | timezone | id | admin1 | admin2 | countryname | latlon | currency | continent";
 
     private GeonameFunctions() {}
 
     @ScalarFunction("geoname")
-    @Description("Returns geoname attribute for a given latitude and longitude. Attribute can be one of: " + attributes_desc)
-    @SqlType(StandardTypes.VARCHAR)
+    @Description("Returns nearest geoname from it's centroid to a given latitude and longitude")
+    @SqlType(VARCHAR)
     @SqlNullable
-    public static Slice geoname (
+    public static Slice geoname(
+            @SqlType(StandardTypes.DOUBLE) double lat,
+            @SqlType(StandardTypes.DOUBLE) double lon
+    ) {
+        City city = new Atlas().find(lat, lon);
+        return (city == null) ? null : utf8Slice(city.toString());
+    }
+
+    @ScalarFunction("geoname")
+    @Description("Returns nearest geoname attribute from it's centroid to a given latitude and longitude. Attribute can be one of: " + attributes_desc)
+    @SqlType(VARCHAR)
+    @SqlNullable
+    public static Slice geoname(
             @SqlType(StandardTypes.DOUBLE) double lat,
             @SqlType(StandardTypes.DOUBLE) double lon,
-            @SqlType(StandardTypes.VARCHAR) Slice attr
+            @SqlType(VARCHAR) Slice attr
             ) {
         City city = new Atlas().find(lat, lon);
         return (city == null) ? null : utf8Slice(getTypeValue(city, attr.toStringUtf8().toLowerCase()));
     }
 
     private static String getTypeValue(City city, String attr) {
-        if (attr.equals("countrycode") || attr.equals("country_code")) {
+        if (attr.equals("country") || attr.equals("countrycode") || attr.equals("country_code")) {
             return city.countryCode;
         }
         if (attr.equals("city") || attr.equals("cityname") || attr.equals("city_name")) {
@@ -49,6 +62,23 @@ public class GeonameFunctions {
         if (attr.equals("admin2")) {
             return city.admin2;
         }
+        if (attr.equals("id")) {
+            return String.valueOf(city.geoNameId);
+        }
+        if (attr.equals("latlon")) {
+            return format("%f,%f", city.latitude, city.longitude);
+        }
+        if (attr.equals("countryname") || attr.equals("country_name")) {
+            return city.countryName;
+        }
+        if (attr.equals("currency") || attr.equals("currencycode") || attr.equals("currency_code") ) {
+            return city.currencyCode;
+        }
+        if (attr.equals("continent")) {
+            return city.continent;
+        }
         throw new PrestoException(INVALID_FUNCTION_ARGUMENT, format("Valid attributes are: %s", attributes_desc));
     }
+
+
 }
--- src/test/java/io/prestosql/geoname/functions/GeonameFunctionsTest.java
+++ src/test/java/io/prestosql/geoname/functions/GeonameFunctionsTest.java
@@ -7,9 +7,22 @@ import static org.testng.Assert.*;
 
 public class GeonameFunctionsTest {
 
+
     @Test
     public void testGeoname() {
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227).toStringUtf8(),
+                "{\"geoNameId\":295277,\"name\":\"Eilat\",\"latitude\":29.55805,\"longitude\":34.94821,\"countryCode\":\"IL\",\"timeZone\":\"Asia/Jerusalem\",\"admin1\":\"Southern District\",\"admin2\":null,\"countryName\":\"Israel\",\"capital\":\"Jerusalem\",\"countryArea\":20770.0,\"countryPopulation\":7353985,\"continent\":\"AS\",\"currencyCode\":\"ILS\",\"currencyName\":\"Shekel\"}");
+   }
+
+    @Test
+    public void testGeonameWithAttribute() {
         assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("countrycode")).toStringUtf8(), "IL");
         assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("city")).toStringUtf8(), "Eilat");
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("id")).toStringUtf8(), "295277");
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("latlon")).toStringUtf8(), "29.558050,34.948210");
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("countryname")).toStringUtf8(), "Israel");
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("currency")).toStringUtf8(), "ILS");
+        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("continent")).toStringUtf8(), "AS");
     }
+
 }
