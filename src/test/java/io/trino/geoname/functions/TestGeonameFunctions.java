bf8d45e9988af7071ded9698307856ff34ae64eb
Guy Cohen <guyco33@gmail.com>
2023-12-20 12:37:52 +0200

    Convert testNG to Junit

--- src/test/java/io/trino/geoname/functions/TestGeonameFunctions.java
+++ src/test/java/io/trino/geoname/functions/TestGeonameFunctions.java
@@ -1,34 +1,56 @@
 package io.trino.geoname.functions;
 
-import io.trino.operator.scalar.AbstractTestFunctions;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import io.trino.sql.query.QueryAssertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.parallel.Execution;
 
 import static io.airlift.slice.Slices.utf8Slice;
-import static org.testng.Assert.assertEquals;
+import static io.trino.geoname.functions.GeonameFunctions.geoname;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
-public class TestGeonameFunctions { // extends AbstractTestFunctions {
+@TestInstance(PER_CLASS)
+@Execution(CONCURRENT)
+public class TestGeonameFunctions
+{
+    private QueryAssertions assertions;
 
-//    @BeforeClass
-//    public void setupClass()
-//    {
-//        registerScalar(GeonameFunctions.class);
-//    }
+    public TestGeonameFunctions()
+    {
+        assertions = new QueryAssertions();
+        assertions.addPlugin(new GeonameFunctionsPlugin());
+    }
+
+    @AfterAll
+    public void teardown()
+    {
+        assertions.close();
+    }
 
     @Test
-    public void testGeoname() {
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227).toStringUtf8(),
-                "{\"geoNameId\":295277,\"name\":\"Eilat\",\"latitude\":29.55805,\"longitude\":34.94821,\"countryCode\":\"IL\",\"timeZone\":\"Asia/Jerusalem\",\"admin1\":\"Southern District\",\"admin2\":null,\"countryName\":\"Israel\",\"capital\":\"Jerusalem\",\"countryArea\":20770.0,\"countryPopulation\":7353985,\"continent\":\"AS\",\"currencyCode\":\"ILS\",\"currencyName\":\"Shekel\"}");
-   }
+    public void testGeoname()
+    {
+        String expected = "{\"geoNameId\":295277,\"name\":\"Eilat\",\"latitude\":29.55805,\"longitude\":34.94821,\"countryCode\":\"IL\",\"timeZone\":\"Asia/Jerusalem\",\"admin1\":\"Southern District\",\"admin2\":null,\"countryName\":\"Israel\",\"capital\":\"Jerusalem\",\"countryArea\":20770.0,\"countryPopulation\":7353985,\"continent\":\"AS\",\"currencyCode\":\"ILS\",\"currencyName\":\"Shekel\"}";
+        assertEquals(geoname(29.896322, 35.059227).toStringUtf8(),
+                expected);
+
+        assertThat(assertions.expression("geoname(29.896322, 35.059227)"))
+                .isEqualTo(expected);
+    }
 
     @Test
-    public void testGeonameWithAttribute() {
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("countrycode")).toStringUtf8(), "IL");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("city")).toStringUtf8(), "Eilat");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("id")).toStringUtf8(), "295277");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("latlon")).toStringUtf8(), "29.558050,34.948210");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("countryname")).toStringUtf8(), "Israel");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("currency")).toStringUtf8(), "ILS");
-        assertEquals(GeonameFunctions.geoname(29.896322, 35.059227, utf8Slice("continent")).toStringUtf8(), "AS");
+    public void testGeonameWithAttribute()
+    {
+        assertThat(assertions.expression("geoname(29.896322, 35.059227, 'countrycode')")).isEqualTo("IL");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("city")).toStringUtf8(), "Eilat");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("id")).toStringUtf8(), "295277");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("latlon")).toStringUtf8(), "29.558050,34.948210");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("countryname")).toStringUtf8(), "Israel");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("currency")).toStringUtf8(), "ILS");
+        assertEquals(geoname(29.896322, 35.059227, utf8Slice("continent")).toStringUtf8(), "AS");
     }
 }
